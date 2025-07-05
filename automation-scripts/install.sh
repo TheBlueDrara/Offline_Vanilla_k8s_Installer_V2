3667642d854fc91a4a49e640732053f7c117f223
soony0737@gmail.com <soony0737@gmail.com>
2025-07-05 12:36:04 +0300

    created a script to clean vagrant enviroment to reset vms for pocs

---
+++ automation-scripts/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+##################### Start Safe Header ########################
+#Developed by Alex Umansky aka TheBlueDrara
+#Porpuse to install k8s vanilla localy in offline, init a control_plane,
+#and join the second vm, deploy, config a worker node and join to the cluster
+#Date 4.7.25
+set -o errexit
+set -o nounset
+set -o pipefail
+#################### End Safe Header ###########################
